72630afe99cf1a021edfcc1698b84c48c1d7453a
slawton <seanlawton3@gmai.com>
2022-01-26 17:18:22 +0000

    plausible analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return (
     <>
+      
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=G-2ZCBF4KS17`}
@@ -26,6 +27,7 @@ export default function App({ Component, pageProps }) {
           <meta name="keywords" content="React, Nextjs, JavaScript, Python, PHP" />
           <meta name="author" content="Sean Lawton" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <script defer data-domain="seanlawton.com" src="https://plausible.io/js/plausible.js"></script>
       </Head>
       <Theme>
         <Component {...pageProps} />
